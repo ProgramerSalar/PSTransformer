94a5bd650dcad37d2266631e70781fe2e6bc5290
ProgramerSalar <manishkumar60708090@gmail.com>
2024-09-13 18:04:34 +0530

    add the duffion functin

---
+++ diffusion/__init__.py
@@ -0,0 +1,11 @@
+from .encoder import VAE_Encoder
+from .decoder import VAE_AttentionBlock, VAE_Decoder
+from .attention import SelfAttention, CrossAttention
+from .clip import CLIPEmbedding, CLIPLayer, CLIP
+from .ddpm import DDPMSampler
+
+
+from .pipeline import generate, rescale, get_time_embedding
+from .model_loader import preload_models_from_standard_weights
+
+
